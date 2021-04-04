c25950b21152120b12a2f17031e0213e5df65d22
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-04 20:25:26 -0300

    add aula 18 operadores lógicos

---
+++ Lógica Programacion Apreende Programar en Qualquer Linguagem(Espanhol)/sessão2/aula_18/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+package sessão2.aula_18;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        boolean v = true;
+        boolean f = false;
+        System.out.println("v && f =" + (v && f));
+        System.out.println("v || f =" + (v || f));
+        System.out.println("!(v && f) =" +!(v&&f));
+    }
+}
